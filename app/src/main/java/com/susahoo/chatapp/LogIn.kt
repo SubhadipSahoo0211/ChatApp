1ffc8f2a5893f247a277302ad86f8c471ea65cfc
Subhadip Sahoo <subhadipsahoo1230@gmail.com>
2022-11-04 20:10:09 +0530

    Showing the friends Name on App

--- app/src/main/java/com/susahoo/chatapp/LogIn.kt
+++ app/src/main/java/com/susahoo/chatapp/LogIn.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LogIn : AppCompatActivity() {
 
@@ -14,9 +16,15 @@ class LogIn : AppCompatActivity() {
     private lateinit var btnLogIn: Button
     private lateinit var btnSignUp: Button
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        supportActionBar?.hide()
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edt_email)
         edtPassword = findViewById(R.id.edt_password)
@@ -28,5 +36,26 @@ class LogIn : AppCompatActivity() {
             startActivity(intent)
         }
 
+        btnLogIn.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            login(email,password)
+        }
+
+    }
+    private  fun login(email: String, password: String){
+        // Logic for logging in user
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Code for logging in
+                    val intent = Intent(this@LogIn, MainActivity::class.java)
+                    finish()
+                    startActivity(intent)
+                } else {
+                    Toast.makeText(this@LogIn,"User does not exit", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
